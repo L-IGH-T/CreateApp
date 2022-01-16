3785178f2e26b1c0928c64cda499a44456e43ac5
L-IGH-T <majo.polonec@gmail.com>
2022-01-16 04:24:21 -0800

    v.1.1.0

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+## Getting started
+
+This CLI is only for clone my repositories.
+
+```console
+  npx L-IGH-T/CreateApp my-app -repo:{name of repository}
+```
+
+### All options
+
+|                               Option                              | Description                                                                                                                              |
+| :---------------------------------------------------------------: | :-------------------------------------
+|  -npm                                                             | Force install with npm. default: yarn
+|  -repo:                                                           | Name of repository to clone.
--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-const { execSync } = require('child_process');
+const { execSync,spawnSync } = require('child_process');
 const path = require('path');
 const fs = require('fs');
 
@@ -16,8 +16,9 @@ if (process.argv.length < 3) {
 const projectName = process.argv[2];
 const currentPath = process.cwd();
 let projectPath,git_repo;
-let db = process.argv.find((arg,i) => arg.includes('-b:'));
+let db = process.argv.find((arg,i) => arg.includes('-b:') || arg.includes('-db'));
 let repo = process.argv.find((arg,i) => arg.includes('-repo:'));
+const NPM = process.argv.find((arg,i) => arg.includes('-npm'));
 if(repo && repo != "")
 {
     repo = repo.replace('-repo:','').trim();
@@ -32,7 +33,9 @@ try {
     fs.mkdirSync(projectPath);
   } catch (err) {
     if (err.code === 'EEXIST') {
-      console.log(`The file ${projectName} already exist in the current directory, please give it another name.`);
+      console.log(`The file ${projectName} already exist in the current directory.`);
+      postProcess();
+      console.log('     The installation is \x1b[32mdone\x1b[0m and ready to use !');
     } else {
       console.log(err);
     }
@@ -46,27 +49,83 @@ async function main() {
     execSync(`git clone --depth 1 ${git_repo} ${projectPath}`,{stdio: 'inherit'});
 
     process.chdir(projectPath);
+    editJson(projectPath,projectName);
     console.log('');
     console.log('     \x1b[32mInstalling dependencies...\x1b[0m');
-    execSync('npm install',{stdio: 'inherit'});
+    let run = 'npm';
+    if(checkYarn() && !NPM)
+    {
+      run = "yarn";
+    }
+    execSync(run + ' install',{stdio: 'inherit'});
 
     console.log('     \x1b[31mRemoving useless files\x1b[0m');
     execSync('npx rimraf ./.git',{stdio: 'inherit'});
-    fs.rmdirSync(path.join(projectPath, 'bin'), { recursive: true});
-    if(db)
+    let bin = path.join(projectPath, 'bin');
+    if(fs.existsSync(bin))
     {
-        db = db.replace('-b:','').trim();
-        let folderName = db || "backend";
-        git_repo = "https://github.com/L-IGH-T/backend.git";
-        console.log('');
-        console.log('     \x1b[34mDownloading backend...\x1b[0m');
-        execSync(`git clone --depth 1 ${git_repo} ./${folderName}`,{stdio: 'inherit'});
-        console.log('     \x1b[31mRemoving useless files\x1b[0m');
-        execSync(`npx rimraf ./${folderName}/.git`,{stdio: 'inherit'});
+      fs.rmdirSync(bin, { recursive: true});
     }
+
+    /* Install things */
+    postProcess();
+
     console.log('     The installation is \x1b[32mdone\x1b[0m and ready to use !');
   } catch (error) {
     console.log(error);
   }
 }
 main();
+
+
+async function postProcess(){
+  try {
+    
+    console.log('     \x1b[32mStarting postProcess...\x1b[0m');
+    process.chdir(projectPath);
+    if(db)
+    {
+        db = db.replace(/-b:|-db:|-db/g,'').trim();
+        let folderName = db || "backend";
+        if(!fs.existsSync(folderName))
+        {
+
+          git_repo = "https://github.com/L-IGH-T/backend.git";
+          console.log('');
+          console.log('     \x1b[34mDownloading backend...\x1b[0m');
+          execSync(`git clone --depth 1 ${git_repo} ./${folderName}`,{stdio: 'inherit'});
+          console.log('     \x1b[31mRemoving useless files\x1b[0m');
+          execSync(`npx rimraf ./${folderName}/.git`,{stdio: 'inherit'});
+        }
+        else
+        {
+          console.log('');
+          console.log('     \x1b[31mFolder already exist. \n  \x1b[34m   Skipping...\x1b[0m \n')
+        }
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+function editJson(packagesDir,name) { 
+  const packageJson = path.join(packagesDir, 'package.json');
+  if (fs.existsSync(packageJson)) {
+    const json = JSON.parse(fs.readFileSync(packageJson, 'utf8'));
+    json.name = nameSanitize(name);
+    fs.writeFileSync(packageJson, JSON.stringify(json, null, 2), 'utf8');
+  }
+}
+
+function nameSanitize(string){
+  return string.charAt(0).toLowerCase() + string.slice(1).replace(/[A-Z]/g, function(e) {
+    return '-'+e.toLowerCase();
+  }).replace(/[0-9]/g, '');
+}
+
+function checkYarn(){
+  const packageName = "yarn";
+  const whyBuffer = spawnSync ('npm', ['list','-g',packageName]);
+  return whyBuffer.stdout.toString().includes(packageName);
+}
